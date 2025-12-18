b6e5080cecd2020703bd4d4a73a5d838b6ee5395
Duhyadi <duhyadioliva@gmail.com>
2025-12-18 07:42:28 -0600

    rename and delete files

--- figure_4_admixture/bin/admixture.sh
+++ figure_4_admixture/bin/admixture.sh
@@ -5,7 +5,7 @@ mixplates="/home/duhyadi/Documents/paper1/admixture/download_cluster/file_plink/
 out="/home/duhyadi/Documents/paper1/admixture/download_cluster/file_plink/"
 
 # convert to plink format  
-#plink --vcf $mixplates --make-bed --out $out/mixplates_filtered
+# plink --vcf $mixplates --make-bed --out $out/mixplates_filtered
 
 # run admixture for K=2 to k=4 10x 
 for K in {2..4}; do
